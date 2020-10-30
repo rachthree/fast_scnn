62801fe46d055bf9f80b875bd5279d1ad70b7ae7
Craig <46288912+rachthree@users.noreply.github.com>
2020-10-29 21:00:56 -0700

    Flexibility to conserve memory (#3)
    * Initial commit
    
    * Allowing resizing of labels (resize to input image size would be done after inference)
    
    Co-authored-by: rachthree <rachthree@users.noreply.github.com>

--- fast_scnn/utils/config.py
+++ fast_scnn/utils/config.py
@@ -3,21 +3,26 @@ import time
 from copy import deepcopy
 from pathlib import Path
 
-TRAIN_DEFAULTS = {'train_dir': r"E:\datasets\Cityscapes\leftImg8bit_trainvaltest\leftImg8bit\train",
-                  'train_label_dir': r"E:\datasets\Cityscapes\gtFine\train",
-                  'val_dir': r"E:\datasets\Cityscapes\leftImg8bit_trainvaltest\leftImg8bit\val",
-                  'val_label_dir': r"E:\datasets\Cityscapes\gtFine\val",
+TRAIN_DEFAULTS = {'train_dir': "E:/datasets/Cityscapes/leftImg8bit_trainvaltest/leftImg8bit/train",
+                  'train_label_dir': "E:/datasets/Cityscapes/gtFine/train",
+                  'val_dir': "E:/datasets/Cityscapes/leftImg8bit_trainvaltest/leftImg8bit/val",
+                  'val_label_dir': "E:/datasets/Cityscapes/gtFine/val",
                   'save_dir': str(Path(__file__).parent.parent.parent.joinpath('model_training')),
                   'sess_name': time.ctime(time.time()).replace(':', '.'),
                   'seed': None,
                   'epochs': 1000,
                   'early_stopping': False,
                   'batch_size': 12,
+                  'prefetch': 1,
+                  'num_parallel_calls': 1,
+                  'autotune_dataset': False,
                   'end_learning_rate': 0.00001,
                   'input_names': ['input_layer'],
                   'output_names': ['output', 'ds_aux', 'gfe_aux'],
-                  'autotune_dataset': False,
-                  'aux_resize': None}
+                  'aux_resize': None,
+                  'float_type': 'float32',
+                  'resize_label': False,
+                  }
 
 def load_config(config_filepath):
     with open(config_filepath, 'r') as f:
--- fast_scnn/utils/dataset.py
+++ fast_scnn/utils/dataset.py
@@ -8,8 +8,9 @@ AUTO = tf.data.experimental.AUTOTUNE
 
 
 class CityScapesDataset(object):
-    def __init__(self, *, data_dir, label_dir, prefetch=1, batch_size=32, seed=None, num_parallel_calls=1, img_norm=True,
-                 output_names=None, resize_aux=None, augment=False, autotune=False, data_suffix='_leftImg8bit', label_suffix='_gtFine_labelIds'):
+    def __init__(self, *, data_dir, label_dir, prefetch=1, batch_size=16, seed=None, num_parallel_calls=1, img_norm=True,
+                 output_names=None, resize_aux=None, augment=False, autotune=False, float_type='float32', resize_label=False,
+                 data_suffix='_leftImg8bit', label_suffix='_gtFine_labelIds'):
         self.data_dir = data_dir
         self.data_suffix = data_suffix
         self.label_dir = label_dir
@@ -22,6 +23,7 @@ class CityScapesDataset(object):
         self.output_names = output_names
         self.augment = augment
         self.resize_aux = resize_aux
+        self.resize_label = resize_label
 
         if self.seed is not None:
             np.random.seed(self.seed)
@@ -30,12 +32,37 @@ class CityScapesDataset(object):
             self.prefetch = AUTO
             self.num_parallel_calls = AUTO
 
+        if float_type == 'float32':
+            self.tf_float = tf.float32
+
+        elif float_type == 'float16':
+            self.tf_float = tf.float16
+
+        else:
+            raise ValueError(f"float_type {float_type} is not recognized")
+
         # Random color channels noise - only to image - hue, saturation, contrast?
         # Random brightness - only to image
-        self.color_ops = [lambda img: tf.image.random_hue(img, 0.2, seed=seed),
-                          lambda img: tf.image.random_saturation(img, lower=0.5, upper=1.5, seed=seed),
-                          lambda img: tf.image.random_contrast(img, lower=0.5, upper=1.5, seed=seed),
-                          lambda img: tf.image.random_brightness(img, max_delta=32./255., seed=seed)]
+        self.color_ops = [self.apply_random_hue,
+                          self.apply_random_saturation,
+                          self.apply_random_contrast,
+                          self.apply_random_brightness]
+
+    @staticmethod
+    def apply_random_hue(img, seed=None):
+        return tf.image.random_hue(img, 0.2, seed)
+
+    @staticmethod
+    def apply_random_saturation(img, seed=None):
+        return tf.image.random_saturation(img, lower=0.5, upper=1.5, seed=seed)
+
+    @staticmethod
+    def apply_random_contrast(img, seed=None):
+        return tf.image.random_contrast(img, lower=0.5, upper=1.5, seed=seed)
+
+    @staticmethod
+    def apply_random_brightness(img, seed=None):
+        return tf.image.random_brightness(img, max_delta=32./255., seed=seed)
 
     def parse_data(self, filepath):
         label_filepath = tf.strings.regex_replace(filepath, self.data_dir.replace("\\", "\\\\"), self.label_dir.replace("\\", "\\\\"))
@@ -44,14 +71,14 @@ class CityScapesDataset(object):
         img_str = tf.io.read_file(filepath)
         img = tf.image.decode_png(img_str, channels=0)
         img = img[..., :3]
-        img = tf.cast(img, tf.float32)
+        img = tf.cast(img, self.tf_float)
         img_shape = tf.shape(img)
 
         label_str = tf.io.read_file(label_filepath)
         label = tf.image.decode_png(label_str, channels=0)
         label = label[..., 0]
         label = tf.reshape(label, (img_shape[0], img_shape[1]))  # tf.squeeze results in unknown shape
-        label = tf.cast(label, tf.int32)
+        label = tf.cast(label, tf.uint8)
 
         return img, label
 
@@ -68,27 +95,32 @@ class CityScapesDataset(object):
             img = tf.image.flip_left_right(img)
             label = tf.image.flip_left_right(label)
 
+        if self.resize_label and not self.augment:
+            label = tf.image.resize(label, self.resize_label, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, preserve_aspect_ratio=True)
+
+        label = tf.cast(label, self.tf_float)
+
         return img, label
 
     def augment_batch(self, img_batch, output_batch):
         img_batch_shape = tf.shape(img_batch)
         n = img_batch_shape[0]
-        h = tf.cast(img_batch_shape[1], tf.float32)
-        w = tf.cast(img_batch_shape[2], tf.float32)
+        h = tf.cast(img_batch_shape[1], self.tf_float)
+        w = tf.cast(img_batch_shape[2], self.tf_float)
 
         # Random resizing - both image and label
-        scale = tf.random.uniform([], minval=0.5, maxval=2.0, dtype=tf.float32, seed=self.seed)
+        scale = tf.random.uniform([], minval=0.5, maxval=2.0, dtype=self.tf_float, seed=self.seed)
         resize_shape = tf.cast([scale*h, scale*w], tf.int32)
-        img_aug = tf.image.resize(img_batch, resize_shape, method=tf.image.ResizeMethod.BILINEAR,
-                                  preserve_aspect_ratio=True)
-        label_aug = tf.image.resize(output_batch, resize_shape, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
-                                    preserve_aspect_ratio=True)
+        img_aug = tf.cast(tf.image.resize(img_batch, resize_shape, method=tf.image.ResizeMethod.BILINEAR,
+                                          preserve_aspect_ratio=True), self.tf_float)
+        label_aug = tf.cast(tf.image.resize(output_batch, resize_shape, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
+                                            preserve_aspect_ratio=True), self.tf_float)
 
         # Random translation/crop - both image and label
 
         if scale > 1.0:
             # crop to input shape
-            io_cat = tf.concat([img_batch, output_batch], axis=-1)
+            io_cat = tf.concat([img_aug, label_aug], axis=-1)
             n_dim = tf.shape(io_cat)[-1]
 
             io_cat = tf.image.random_crop(io_cat, (n, img_batch_shape[1], img_batch_shape[2], n_dim), seed=self.seed)
@@ -96,15 +128,18 @@ class CityScapesDataset(object):
 
         elif scale < 1.0:
             # resize using padding
-            img_aug = tf.image.resize_with_crop_or_pad(img_batch, img_batch_shape[1], img_batch_shape[2])
-            label_aug = tf.image.resize_with_crop_or_pad(output_batch, img_batch_shape[1], img_batch_shape[2])
+            img_aug = tf.cast(tf.image.resize_with_crop_or_pad(img_batch, img_batch_shape[1], img_batch_shape[2]), self.tf_float)
+            label_aug = tf.cast(tf.image.resize_with_crop_or_pad(output_batch, img_batch_shape[1], img_batch_shape[2]), self.tf_float)
 
+        if self.resize_label:
+            label_aug = tf.cast(tf.image.resize(label_aug, self.resize_label, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR,
+                                                preserve_aspect_ratio=True), self.tf_float)
 
         # GaussianNoise layer to be added to model directly
         # Order of operations does matter... randomize operations
         np.random.shuffle(self.color_ops)
         for op in self.color_ops:
-            img_aug = op(img_aug)
+            img_aug = op(img_aug, self.seed)
 
         return img_aug, label_aug
 
